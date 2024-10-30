3fe8a0b7f4b363de1df4108ce1f41057301c0c12
Boguchal <259574@student.pwr.edu.pl>
2024-10-30 12:10:20 +0100

    dodanie maina z maszynami stanow

---
+++ global.h
@@ -0,0 +1,24 @@
+#ifndef __GLOBAL_H
+#define __GLOBAL_H
+
+enum states{
+    init,
+    menu,
+    measure,
+    calibration,
+    debug
+};
+enum Menu{
+    mmeasure,
+    mcalibration,
+    mdebug
+};
+
+enum states state = init;
+char enter = 0;
+char button_next_value = 0;
+char button_enter_value = 0;
+enum Menu menu_selection = mmeasure;
+char number_of_menu_options = 3;//measure,calibration,debug
+
+#endif
